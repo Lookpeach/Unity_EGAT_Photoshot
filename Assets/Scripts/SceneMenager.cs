1098905137fe5cf0741ba7a1283daec6e08c4fdc
user <peach04.ascript@gmail.com>
2019-10-09 14:58:30 +0700

    finnish

---
+++ Assets/Scripts/SceneMenager.cs
@@ -0,0 +1,88 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+
+public class SceneMenager : MonoBehaviour
+{
+    public int numOfFrame = 0;
+    public GameObject canvasStart, canvasFrame, canvasCountDown, canvasTakePhoto, canvasOutPut, canvasThank;
+    public float timeLeft = 5.0f;
+    public Text startText; // used for showing countdown from 3, 2, 1 
+    public List<Sprite> imgCount = new List<Sprite>();
+    public GameObject img;
+    public bool flagCountdown = false;
+
+    void Update()
+    {
+        
+        if (flagCountdown == true)
+        {
+            CountDonw();
+        }
+        
+    }
+
+    public void StartButton()
+    {
+        canvasStart.SetActive(false);
+        canvasFrame.SetActive(true);
+        canvasCountDown.SetActive(false);
+        canvasTakePhoto.SetActive(false);
+        canvasOutPut.SetActive(false);
+        canvasThank.SetActive(false);
+    }
+    public void SeletedFrame(int num)
+    {
+        if(num == 1) //frame 1
+        {
+            numOfFrame = 1;
+        }
+        else if(num == 2) //frame 2
+        {
+            numOfFrame = 2;
+        }
+        else if(num == 3) //frame 3
+        {
+            numOfFrame = 3;
+        }
+        else if(num == 4) //click ok
+        {
+            canvasStart.SetActive(false);
+            canvasFrame.SetActive(false);
+            canvasCountDown.SetActive(true);
+            canvasTakePhoto.SetActive(false);
+            canvasOutPut.SetActive(false);
+            canvasThank.SetActive(false);
+            flagCountdown = true;
+        }
+    }
+    public void CountDonw()
+    {
+        timeLeft -= Time.deltaTime;
+        Debug.Log("time" + timeLeft);
+        startText.text = (timeLeft).ToString("0");
+        if (startText.text == "3")
+        {
+            img.GetComponent<Image>().sprite = imgCount[0];
+        }
+        else if (startText.text == "2")
+        {
+            img.GetComponent<Image>().sprite = imgCount[1];
+        }
+        else if (startText.text == "1")
+        {
+            img.GetComponent<Image>().sprite = imgCount[2];
+        }
+        else if (startText.text == "0")
+        {
+            img.GetComponent<Image>().sprite = imgCount[3];
+        }
+        if (timeLeft < 0)
+        {
+            AsyncOperation async = SceneManager.LoadSceneAsync(1);
+            flagCountdown = false;
+        }   
+    }
+}
---
+++ Assets/Scripts/module_result.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using System.IO;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+
+public class module_result : MonoBehaviour {
+
+    public Texture2D photo;
+    public Image newPhoto;
+    public string get_code, filePath;
+	Texture2D myTexture;
+    public GameObject canvasOutput, canvasThank;
+
+	void Start () {
+		get_code = PlayerPrefs.GetString ("code");
+		Debug.Log ("photos/" + get_code + ".png");
+		myTexture = null;
+		byte[] fileData;
+		filePath = "photos/" + get_code + ".png";
+		if (File.Exists (filePath)) {
+			Debug.Log ("get");
+			fileData = File.ReadAllBytes (filePath);
+			myTexture = new Texture2D (2, 2);
+			myTexture.LoadImage (fileData);
+			GameObject rawImage = GameObject.Find ("RawImage");
+			rawImage.GetComponent<RawImage> ().texture = myTexture;
+		}
+	}
+    public void CancleBtn()
+    {
+        AsyncOperation async = SceneManager.LoadSceneAsync(1);
+    }
+    public void OKbtn()
+    {
+        canvasOutput.SetActive(false);
+        canvasThank.SetActive(true);
+    }
+}
